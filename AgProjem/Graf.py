306621271dd3d3f74f7dfd2cd94b0b189d932cb2
Ismailkrblt <162838395+Ismailkrblt@users.noreply.github.com>
2024-03-29 12:11:07 +0300

    upload

---
+++ AgProjem/Graf.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Veri setini yükle
+df = pd.read_csv('/kaggle/input/trojan-detection-csv/Trojan_Detection.csv')  
+
+# Veri setinin dosya yolunu düzenleyin
+df = pd.DataFrame(data)
+
+# Trojen ve benign dosyaların sayısını source IP'ye göre karşılaştır
+plt.figure(figsize=(10, 6))
+sns.countplot(data=df, x='Source IP', hue='Class')
+plt.title('Trojen ve Benign Dosyaların Source IP Karşılaştırması')
+plt.xlabel('Source IP')
+plt.ylabel('Dosya Sayısı')
+plt.show()
