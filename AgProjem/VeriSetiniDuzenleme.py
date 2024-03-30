c3e4a942c69554457a45d2a610f333eefad5f480
Ismailkrblt <162838395+Ismailkrblt@users.noreply.github.com>
2024-03-31 00:36:32 +0300

    upload

---
+++ AgProjem/VeriSetiniDuzenleme.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+df = pd.read_csv("Trojan_Detection.csv", sep = r',', skipinitialspace = True)
+
+df = df.dropna()
+
+df.drop(["Unnamed: 0"], axis = 1).values
+
+df = df.replace("Trojan", 1)
+df = df.replace("Benign", 0)
+
+from sklearn import preprocessing
+
+number = preprocessing.LabelEncoder()
+
+df["Flow ID"] = number.fit_transform(df["Flow ID"])
+df["Source IP"] = number.fit_transform(df["Source IP"])
+df["Destination IP"] = number.fit_transform(df["Destination IP"])
+df["Timestamp"] = number.fit_transform(df["Timestamp"])
+
+df.head()
