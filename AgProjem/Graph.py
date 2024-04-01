c976f38e70656c9cc627b22ce07677e6915a8095
Ismailkrblt <162838395+Ismailkrblt@users.noreply.github.com>
2024-04-01 14:15:17 +0300

    upload

--- AgProjem/Graph.py
+++ AgProjem/Graph.py
@@ -1,22 +1,23 @@
 import numpy as np
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
 # Veri setini yükle
-data = pd.read_csv("/kaggle/input/trojan-detection-csv/Trojan_Detection.csv")
+data = pd.read_csv("Trojan_Detection.csv")
 
 # Kaynak ve hedef IP adreslerini ve portları listeye dönüştür
-kaynak_ipler = data["Source IP"].tolist()
-hedef_ipler = data["Destination IP"].tolist()
-kaynak_portlar = data["Source Port"].tolist()
-hedef_portlar = data["Destination Port"].tolist()
+kaynak_ipler = data[" Source IP"].tolist()
+hedef_ipler = data[" Destination IP"].tolist()
+kaynak_portlar = data[" Source Port"].tolist()
+hedef_portlar = data[" Destination Port"].tolist()
 
 # Graf oluştur
 G = nx.Graph()
 
 # Her bağlantıyı bir kenar olarak ekle
 for i in range(len(kaynak_ipler)):
-    G.add_edge(kaynak_ipler[i]+":"+str(kaynak_portlar[i]), hedef_ipler[i]+":"+str(hedef_portlar[i]))
+    G.add_edge(f"{kaynak_ipler[i]}:{kaynak_portlar[i]}", f"{hedef_ipler[i]}:{hedef_portlar[i]}")
 
 # Her köşeye "trojen" veya "benign" etiketi ekle
 for i in range(len(data)):
@@ -27,21 +28,12 @@ for i in range(len(data)):
         G.nodes[kaynak_ipler[i]+":"+str(kaynak_portlar[i])]["Class"] = "Benign"
         G.nodes[hedef_ipler[i]+":"+str(hedef_portlar[i])]["Class"] = "Benign"
 
-# Graf transformatörünü oluştur
-transformer = nx.GraphTransformer()
-
-# Graf transformatörünü uygula
-transformed_graph = transformer.apply(G)
+# Grafı görselleştir
+plt.figure(figsize=(10, 8))
+pos = nx.spring_layout(G, seed=42)  # Grafı düzenle
+nx.draw(G, pos, with_labels=True, node_size=100, font_size=8, font_color="black")
+plt.title("Trojan ve Benign Bağlantılar")
+plt.show()
 
 # Dönüştürülmüş grafı kaydet
-nx.write_gpickle(transformed_graph, "transformed_graph.gpickle")
-
-# Dönüştürülmüş grafı görselleştirmek için NetworkX'i kullanın
-nx.draw(transformed_graph, with_labels=True)
-for i in range(len(data)):
-    if data["Class"][i] == 1:
-        G.nodes[kaynak_ipler[i]+":"+str(kaynak_portlar[i])]["Class"] = "Trojen"
-        G.nodes[hedef_ipler[i]+":"+str(hedef_portlar[i])]["Class"] = "Trojen"
-    else:
-        G.nodes[kaynak_ipler[i]+":"+str(kaynak_portlar[i])]["Class"] = "Benign"
-        G.nodes[hedef_ipler[i]+":"+str(hedef_portlar[i])]["Class"] = "Benign"
+nx.write_gpickle(G, "transformed_graph.gpickle")
