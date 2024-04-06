8b2397dba8f88706c4e0a092932ec0216a0f5fe9
Ismailkrblt <162838395+Ismailkrblt@users.noreply.github.com>
2024-04-07 00:08:10 +0300

    upload

--- AgProjem/VeriSetiniDuzenleme.py
+++ AgProjem/VeriSetiniDuzenleme.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
 
 df = pd.read_csv("Trojan_Detection.csv", sep = r',', skipinitialspace = True)
 
@@ -17,5 +19,11 @@ df["Flow ID"] = number.fit_transform(df["Flow ID"])
 df["Source IP"] = number.fit_transform(df["Source IP"])
 df["Destination IP"] = number.fit_transform(df["Destination IP"])
 df["Timestamp"] = number.fit_transform(df["Timestamp"])
+
+X = df.drop(["Class"], axis = 1).values
+
+y = df["Class"].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0, test_size = 0.2)
 
 df.head()
